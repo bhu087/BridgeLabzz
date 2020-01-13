a86d8777460b6946dbc5966ad3b1bd5fddb59b61
bhu087 <bhu087@gmail.com>
2020-01-13 12:58:10 +0530

    [Bhushan] Add. First commit Algoritm Programs added with Binary word Search program

---
+++ AlgorithmProj/AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your option\n1 for Binary word search");
+            try
+            {
+                int Option = int.Parse(Console.ReadLine());
+                switch (Option)
+                {
+                    case 1:
+                        BinaryWordSearchProgram.BinaryWordSearch();
+                        break;
+                    default:
+                        Console.WriteLine("You have entered Wrong Option");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You are entered wrong input\nTry again...");
+            }
+        }
+    }
+}
