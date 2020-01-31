024c56cc1d6f4f04d67af8a79c3302f977acc854
bhu087 <bhu087@gmail.com>
2020-01-31 10:25:47 +0530

    [Bhushan] Add. Node for linked List Added

---
+++ OOPsProj/ObjectOrientedPrograms/ObjectOrientedPrograms/Node.cs
@@ -0,0 +1,33 @@
+﻿////...................................
+////<copyright file="Node.cs" company="BridgeLabz">
+////author="Bhushan"
+////</copyright>
+////...................................
+namespace ObjectOrientedPrograms
+{
+    /// <summary>
+    /// This is the Node class.
+    /// </summary>
+    /// <typeparam name="T">T is a generics type</typeparam>
+    public class Node<T>
+    {
+        /// <summary>
+        /// The next Node;
+        /// </summary>
+        public Node<T> Next;
+
+        /// <summary>
+        /// The data
+        /// </summary>
+        public T Data;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node{T}"/> class.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public Node(T data)
+        {
+            this.Data = data;
+        }
+    }
+}
