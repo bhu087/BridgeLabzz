dea7c9b9b59b68e95e858cfd47d67b51731fcb6d
bhu087 <bhu087@gmail.com>
2020-01-31 11:41:26 +0530

    [Bhushan] Add. Stack program added

---
+++ OOPsProj/ObjectOrientedPrograms/ObjectOrientedPrograms/MyStack.cs
@@ -0,0 +1,35 @@
+﻿////...................................
+////<copyright file="MyStack.cs" company="BridgeLabz">
+////author="Bhushan"
+////</copyright>
+////...................................
+namespace ObjectOrientedPrograms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This is my stack
+    /// </summary>
+    class MyStack
+    {
+        /// <summary>
+        /// The transaction details List
+        /// </summary>
+        public static MyLinkedList<string> transactionDetails = new MyLinkedList<string>();
+
+        /// <summary>
+        /// The size of Stack
+        /// </summary>
+        static int size = 0;
+        /// <summary>
+        /// Pushes to list.
+        /// </summary>
+        public void Push(string details)
+        {
+            size++;
+            transactionDetails.Add(details);
+        }
+    }
+}
