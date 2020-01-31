5ed56fbdfcd4dd6f5b4444e27ebda77f86becdac
bhu087 <bhu087@gmail.com>
2020-01-31 15:47:59 +0530

    [Bhushan] Add. Address Book project added

---
+++ OOPsProj/AddressBook/AddressBook/AddressBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        public static void AddressBookMethod()
+        {
+            Utility AddressUtility = new Utility();
+            Console.WriteLine("Welcome to Address Book");
+            Console.WriteLine("1 for Add a person\n2 for Edit a person\n3 for Delete a person");
+            Console.WriteLine("4 for Sort by Names\n5 for sort by ZIP\n6 for print entries");
+            int option = AddressUtility.IntInput();
+            switch (option)
+            {
+                case 6:
+                    AddressUtility.PrintEntries();
+                    break;
+            }
+        }
+    }
+}
---
+++ OOPsProj/AddressBook/AddressBook/AddressObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressObject
+    {
+        public string Name { get; set; }
+        public long MobileNumber { get; set; }
+        public string Company { get; set; }
+    }
+}
