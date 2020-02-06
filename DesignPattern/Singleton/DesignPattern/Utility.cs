f74063e43ded64398a2c6b7e2a7e26d7e6276afa
bhu087 <bhu087@gmail.com>
2020-02-06 13:17:11 +0530

    [Bhushan] Add. Design Pattern Folder Replaced

---
+++ DesignPattern/Singleton/DesignPattern/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    class Utility
+    {
+        public int IntiInput()
+        {
+            int intValue;
+            bool success = int.TryParse(Console.ReadLine(), out intValue);
+            if (success)
+            {
+                return intValue;
+            }
+            else 
+            {
+                Console.WriteLine("Invalid Entry");
+                this.IntiInput();
+                return 0;
+            }
+        }
+    }
+}
