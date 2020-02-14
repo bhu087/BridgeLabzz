2ec3ed3182b78cbdf94da88a76398638d263fd52
bhu087 <bhu087@gmail.com>
2020-02-14 14:19:53 +0530

    [Bhushan] Refactor. Update login page.

--- EmployeeManagementSystem/EmployeeManagement/wwwroot/EmployeeJavaScript.js
+++ EmployeeManagementSystem/EmployeeManagement/wwwroot/EmployeeJavaScript.js
@@ -1,1 +1,20 @@
-﻿
+﻿function activeLogin() {
+	$(document).ready(function () {
+		$("#LoginDiv").show();
+    });
+}
+function deActiveLogin() {
+	$(document).ready(function () {
+		$("#LoginDiv").hide();
+	});
+}
+function activeEmployeeManagement() {
+	$(document).ready(function () {
+		$("#EmployeeManagement").show();
+	});
+}
+function deActiveEmployeeManagement() {
+	$(document).ready(function () {
+		$("#EmployeeManagement").hide();
+	});
+}
