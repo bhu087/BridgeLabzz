bdf6d6dd58f225c83e320fbe52decf73d7074ff2
bhu087 <bhu087@gmail.com>
2020-02-18 17:07:36 +0530

    [Bhushan] Refactor. Employee is updated with get and sets

--- EmployeeManagementSystem/EmployeeManagement/Models/Employee.cs
+++ EmployeeManagementSystem/EmployeeManagement/Models/Employee.cs
@@ -7,5 +7,12 @@ namespace EmployeeManagement.Models
 {
     public class Employee
     {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+        public string Salary { get; set; }
+        public string City { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
     }
 }
