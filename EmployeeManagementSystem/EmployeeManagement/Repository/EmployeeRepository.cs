b1ca4c4d558d2cbea20666a0475ea6633d50c604
bhu087 <bhu087@gmail.com>
2020-02-18 17:42:34 +0530

    [Bhushan] Refactor. Updated Employee Repository

--- EmployeeManagementSystem/EmployeeManagement/Repository/EmployeeRepository.cs
+++ EmployeeManagementSystem/EmployeeManagement/Repository/EmployeeRepository.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Text;
+using System.Configuration;
+using EmployeeManagement.Models;
 
 namespace EmployeeManagement.Repository
 {
-    public class EmployeeRepository
+    public class EmployeeRepository : IEmployeeManagement
     {
+        public SqlConnection con;
 
+        public Employee AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee DeleteEmployee(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
