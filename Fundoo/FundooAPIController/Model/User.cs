dd74079098e90064f9c96f6020b633b69e4cfd33
bhu087 <bhu087@gmail.com>
2020-02-22 16:31:41 +0530

    [Bhushan] Refactor. console.core projects added

---
+++ Fundoo/FundooAPIController/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string mobile { get; set; }
+        public string salary { get; set; }
+        public string city { get; set; }
+    }
+}
