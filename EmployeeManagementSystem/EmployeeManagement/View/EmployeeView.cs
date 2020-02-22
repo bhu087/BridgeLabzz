2b17d201b51d8223ed72ee16989fd8b241393477
bhu087 <bhu087@gmail.com>
2020-02-22 11:35:15 +0530

    [Bhushan] Refactor. Employee view class is commented

--- EmployeeManagementSystem/EmployeeManagement/View/EmployeeView.cs
+++ EmployeeManagementSystem/EmployeeManagement/View/EmployeeView.cs
@@ -1,29 +1,69 @@
-﻿using EmployeeManagement.Models;
-using EmployeeManagement.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿/////------------------------------------------------------------------------
+////<copyright file="EmployeeView.cs" company="BridgeLabz">
+////author="Bhushan"
+////</copyright>
+////-------------------------------------------------------------------------
 
 namespace EmployeeManagement.View
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EmployeeManagement.Models;
+    using EmployeeManagement.Repository;
+
+    /// <summary>
+    /// This is the employee View class
+    /// </summary>
+    /// <seealso cref="EmployeeManagement.View.IEmployeeView" />
     public class EmployeeView : IEmployeeView
     {
-        IEmployeeRepository repository = new EmployeeRepository();
+        /// <summary>
+        /// The repository
+        /// </summary>
+        private IEmployeeRepository repository = new EmployeeRepository();
+
+        /// <summary>
+        /// Logins the specified employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <returns>
+        /// returns boolean value
+        /// </returns>
         public bool Login(Employee employee)
         {
-            bool responseFlag =  this.repository.Login(employee);
+            bool responseFlag = this.repository.Login(employee);
             return responseFlag;
         }
+
+        /// <summary>
+        /// Registers the specified employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
         public void Register(Employee employee)
         {
             this.repository.Register(employee);
         }
+
+        /// <summary>
+        /// Gets the employees.
+        /// </summary>
+        /// <returns>
+        /// returns List of values
+        /// </returns>
         public IEnumerable<Employee> GetEmployees()
         {
             return this.repository.GetEmployees();
         }
 
+        /// <summary>
+        /// Updates the employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <returns>
+        /// returns boolean value
+        /// </returns>
         public bool UpdateEmployee(Employee employee)
         {
             return this.repository.UpdateEmployee(employee);
