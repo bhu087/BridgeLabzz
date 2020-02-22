19b4532e9beee15485840c61509a3ce3f11fb0a6
bhu087 <bhu087@gmail.com>
2020-02-22 11:32:38 +0530

    [Bhushan] Refactor. Employee view interface is commented

--- EmployeeManagementSystem/EmployeeManagement/View/IEmployeeView.cs
+++ EmployeeManagementSystem/EmployeeManagement/View/IEmployeeView.cs
@@ -1,16 +1,45 @@
-﻿using EmployeeManagement.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿/////------------------------------------------------------------------------
+////<copyright file="IEmployeeView.cs" company="BridgeLabz">
+////author="Bhushan"
+////</copyright>
+////-------------------------------------------------------------------------
 namespace EmployeeManagement.View
 {
-    interface IEmployeeView
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EmployeeManagement.Models;
+
+    /// <summary>
+    /// This is the Employee view interface
+    /// </summary>
+    public interface IEmployeeView
     {
+        /// <summary>
+        /// Registers the specified employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
         void Register(Employee employee);
+
+        /// <summary>
+        /// Logins the specified employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <returns>returns boolean value</returns>
         bool Login(Employee employee);
+
+        /// <summary>
+        /// Gets the employees.
+        /// </summary>
+        /// <returns>returns List of values</returns>
         IEnumerable<Employee> GetEmployees();
+
+        /// <summary>
+        /// Updates the employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <returns>returns boolean value</returns>
         bool UpdateEmployee(Employee employee);
     }
 }
