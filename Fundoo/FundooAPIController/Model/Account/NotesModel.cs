dc4ba32c002c086adb8f31a4159ee2992e888186
bhu087 <bhu087@gmail.com>
2020-02-29 18:23:02 +0530

    [Bhushan] Add. Notes Model Added

---
+++ Fundoo/FundooAPIController/Model/Account/NotesModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Account
+{
+    class NotesModel
+    {
+        private int NotesId;
+        private string email;
+        private string title;
+        private string description;
+        private DateTime? createdTime;
+        private DateTime? modifiedTime;
+        private string remainder;
+        private string image;
+        private bool isArchive;
+        private bool isTrash;
+        private bool isPin;
+        private string color;
+    }
+}
