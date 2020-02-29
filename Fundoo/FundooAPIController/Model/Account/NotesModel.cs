206ca459187ca0e71f48b3f64844c545bed31619
bhu087 <bhu087@gmail.com>
2020-02-29 18:33:49 +0530

    [Bhushan] Add. Notes Model properties added

--- Fundoo/FundooAPIController/Model/Account/NotesModel.cs
+++ Fundoo/FundooAPIController/Model/Account/NotesModel.cs
@@ -18,5 +18,18 @@ namespace Model.Account
         private bool isTrash;
         private bool isPin;
         private string color;
+
+        public int NotesId1 { get => NotesId; set => NotesId = value; }
+        public string Email { get => email; set => email = value; }
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public DateTime? CreatedTime { get => createdTime; set => createdTime = value; }
+        public DateTime? ModifiedTime { get => modifiedTime; set => modifiedTime = value; }
+        public string Remainder { get => remainder; set => remainder = value; }
+        public string Image { get => image; set => image = value; }
+        public bool IsArchive { get => isArchive; set => isArchive = value; }
+        public bool IsTrash { get => isTrash; set => isTrash = value; }
+        public bool IsPin { get => isPin; set => isPin = value; }
+        public string Color { get => color; set => color = value; }
     }
 }
