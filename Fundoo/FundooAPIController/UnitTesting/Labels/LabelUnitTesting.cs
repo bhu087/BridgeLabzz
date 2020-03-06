37a2521340529174ea28d68fadaf0ad14ffac447
bhu087 <bhu087@gmail.com>
2020-03-06 19:35:08 +0530

    [Bhushan] Add. Unit testing for Label Added

--- Fundoo/FundooAPIController/UnitTesting/Labels/LabelUnitTesting.cs
+++ Fundoo/FundooAPIController/UnitTesting/Labels/LabelUnitTesting.cs
@@ -1,16 +1,80 @@
 ﻿/////------------------------------------------------------------------------
-////<copyright file="NotesUnitTesting.cs" company="BridgeLabz">
+////<copyright file="LabelUnitTesting.cs" company="BridgeLabz">
 ////author="Bhushan"
 ////</copyright>
 ////-------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace UnitTesting.Labels
 {
-    class LabelUnitTesting
-    {
+    using Manager.Labels;
+    using Model.Account;
+    using Moq;
+    using Repository.IRepo;
+    using Xunit;
 
+    /// <summary>
+    /// This is the label unit testing
+    /// </summary>
+    public class LabelUnitTesting
+    {
+        /// <summary>
+        /// Adds the label test.
+        /// </summary>
+        [Fact]
+        public void AddLabelTest()
+        {
+            var service = new Mock<ILabelRepo>();
+            var label = new LabelManager(service.Object);
+            var labelModel = new LabelModel() {
+                LabelId = 1001,
+                LabelName = "Hello"
+            };
+            var result = label.AddLabel(labelModel);
+            Assert.NotNull(result);
+        }
+
+        /// <summary>
+        /// Deletes the label test.
+        /// </summary>
+        [Fact]
+        public void DeleteLabelTest()
+        {
+            var service = new Mock<ILabelRepo>();
+            var label = new LabelManager(service.Object);
+
+            var result = label.DeleteLabel("Hello");
+            Assert.NotNull(result);
+        }
+
+        /// <summary>
+        /// Deletes the note from label.
+        /// </summary>
+        [Fact]
+        public void DeleteNoteFromLabel()
+        {
+            var service = new Mock<ILabelRepo>();
+            var label = new LabelManager(service.Object);
+            var id = 1003;
+            var result = label.DeleteNoteFromLabel(id);
+            Assert.NotNull(result);
+        }
+
+        /// <summary>
+        /// Updates the label test.
+        /// </summary>
+        [Fact]
+        public void UpdateLabelTest()
+        {
+            var service = new Mock<ILabelRepo>();
+            var label = new LabelManager(service.Object);
+            var id = 1003;
+            var labelName = "Hello";
+            var labelModel = new LabelModel()
+            {
+                LabelId = 1001,
+                LabelName = "Hello"
+            };
+            var result = label.UpdateLabel(id, labelName, labelModel);
+            Assert.NotNull(result);
+        }
     }
 }
