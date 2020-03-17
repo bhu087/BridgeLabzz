6cb616e53937e0cc6c6e48f64515785163e81af6
bhu087 <bhu087@gmail.com>
2020-03-17 09:59:19 +0530

    [Bhushan] Add. Dashboard added

--- FundooFrontEnd/src/app/dash-board/dash-board.component.ts
+++ FundooFrontEnd/src/app/dash-board/dash-board.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashBoardComponent implements OnInit {
  loadDashBoard: boolean = false;
  panelOpenState: boolean = false;
+ collapsed : boolean = false;
   constructor() { }
 
   ngOnInit(): void {
