0c1d35d59d3533fc1da63e9edd48a96fe87abc14
bhu087 <bhu087@gmail.com>
2020-03-18 17:38:23 +0530

    [Bhushan] Add. forget password html

--- FundooFrontEnd/src/app/forget-password/forget-password.component.ts
+++ FundooFrontEnd/src/app/forget-password/forget-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forget-password.component.css']
 })
 export class ForgetPasswordComponent implements OnInit {
+  formBuilder: any;
+  forgetForm: any;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.formBuilder = new FormBuilder;
+      this.forgetForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]] 
+      });
   }
 
 }
